b454b2a2c02bd6f10bc8c719644ea4d8a0ef021b
itw_yuwb <itw_yuwb@tkmail.com>
2022-10-16 12:28:53 +0800

    ✨ feat: 加一

---
+++ array-easy/66.加一.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=javascript
+ *
+ * [66] 加一
+ *
+ * https://leetcode.cn/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (45.89%)
+ * Likes:    1096
+ * Dislikes: 0
+ * Total Accepted:    557.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = [0]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  const lastNum = digits.pop()
+  if (lastNum === 9) {
+    if (digits.length > 0) {
+      digits = plusOne(digits)
+    } else {
+      digits.push(1)
+    }
+    digits.push(0)
+  } else {
+    digits.push(lastNum+1)
+  }
+  return digits
+};
+// @lc code=end
+
