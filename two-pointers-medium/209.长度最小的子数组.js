a7430a9ad81f1563f45d541413fdb53427d3fae1
DecSeven <670739394@qq.com>
2022-11-07 21:25:21 +0800

    ✨ feat(双指针): 双指针-左右指针
    滑动窗口-长度最小的子数组

--- two-pointers-medium/209.长度最小的子数组.js
+++ two-pointers-medium/209.长度最小的子数组.js
@@ -70,26 +70,29 @@
  */
 var minSubArrayLen = function(target, nums) {
     let sum = 0     // 最小子数组的和
-    let result = nums.length + 1  // 最后的结果,子数组一定小于原数组长度
+    // 最后的结果,子数组一定小于原数组长度，为了便于后面比较，这里取一个大于最大长度的值。
+    let result = nums.length + 1  
     let subLen = 0 // 当前子数组长度
     // 滑动窗口
     let start = 0 // 起始位置
     for(let end = 0; end < nums.length; end++) {
+        // 开始遍历，即移动窗口终止位置，放大窗口
+        // 计算当前窗口内元素的和
         sum += nums[end]
-        // 当sum >= target时 , 符合要求的数组
+        // 当sum >= target时 , 窗口内有符合要求的数组，存储子数组长度，并开始移动起始位置
         while(sum >= target){
-            // 计算出此时子数组长度
+            // 计算出此时子窗口内数组的长度
             subLen = end - start + 1
-            // 比较此时子数组长度和result 取小
+            // 比较此时子数组长度和result，取小
             result = result > subLen ? subLen : result
-            // 1.子数组和减去当前起始位置的值 2.右移起始位置
-            sum -= nums[start++]
+            // 子数组和减去当前起始位置的值
+            sum  -= nums[start]
+            // 开始移动窗口起始位置，即缩小窗口
+            start++
         }
     }
-
     // 最后判断如果result > 原数组长度，则说明没有最小子数组返回长度0,反之返回result
     return result > nums.length ? 0 : result
-  
 };
 // @lc code=end
 
